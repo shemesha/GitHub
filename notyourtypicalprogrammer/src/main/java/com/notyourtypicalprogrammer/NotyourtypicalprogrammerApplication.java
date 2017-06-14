cc1cb6e4aedb8945ab5601d7d6f736018981994b
Shemesha Wright <shemeshathomas@yahoo.com>
2017-06-14 13:48:49 -0500

    Initial commit for our first Spring boot project

---
+++ notyourtypicalprogrammer/src/main/java/com/notyourtypicalprogrammer/NotyourtypicalprogrammerApplication.java
@@ -0,0 +1,12 @@
+package com.notyourtypicalprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotyourtypicalprogrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotyourtypicalprogrammerApplication.class, args);
+	}
+}
---
+++ notyourtypicalprogrammer/src/test/java/com/notyourtypicalprogrammer/NotyourtypicalprogrammerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.notyourtypicalprogrammer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotyourtypicalprogrammerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
